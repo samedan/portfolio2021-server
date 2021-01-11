0dba30a90df410df2a37534588c98267db4b117d
Daniel Popescu <same.dan@gmail.com>
2021-01-11 15:37:29 +0100

    Before working FE & BE

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { getPortfolios } = require("../controllers/portfolios");
+const {
+  getPortfolios,
+  getPortfolioById,
+} = require("../controllers/portfolios");
 
 const router = express.Router();
 
+//api/v1/portfolios
 router.get("", getPortfolios);
+
+///api/v1/portfolios/id
+router.get("/:id", getPortfolioById);
 
 module.exports = router;
