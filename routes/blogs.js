1eda9e401e77aa9ed3d90cc6fab0b41348a6f706
Daniel Popescu <same.dan@gmail.com>
2021-01-23 13:58:08 +0100

    Published, Draft Blogs

--- routes/blogs.js
+++ routes/blogs.js
@@ -5,6 +5,7 @@ const {
   getBlogBySlug,
   createBlog,
   updateBlog,
+  getBlogsByUser,
 } = require("../controllers/blogs");
 const { checkJwt, checkRole } = require("../controllers/auth");
 
@@ -12,6 +13,7 @@ const router = express.Router();
 
 // GET api/v1/portfolios
 router.get("", getBlogs);
+router.get("/me", checkJwt, checkRole("admin"), getBlogsByUser); // order is important
 router.get("/:id", getBlogById);
 router.get("/s/:slug", getBlogBySlug);
 
