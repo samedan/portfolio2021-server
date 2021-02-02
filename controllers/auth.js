64c144d0a1b4c7b7efadfd17472b85ffa355954f
Daniel Popescu <same.dan@gmail.com>
2021-02-02 16:20:30 +0100

    Before Heroku

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const request = require("request");
-const config = require("../config/dev");
+const config = require("../config");
 
 // Authentication middleware
 exports.checkJwt = jwt({
